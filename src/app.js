6a37f8af49e5eeb91cbadc133a7eaa59f00595f3
Jaime-35 <missha_89@hotmail.com>
2025-07-03 18:29:01 +0000

    Cambios en el generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,23 +1,53 @@
 import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/rigo-baby.jpg";
+
+// Sonido de clic
+const clickSound = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
 window.onload = () => {
-  document.querySelector('.card').classList.add(generateRandomSuit());
-  document.querySelector('.card').innerHTML = gerateRandomNumber();
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    updateCard(card); // Inicializa
+    card.addEventListener("click", () => {
+      clickSound.play();
+      flipCard(card);
+    });
+  });
 };
 
-let gerateRandomNumber =() => {
-  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-}
-let generateRandomSuit =() =>{
-  let suit = [ "diamond", "spade", "heart", "club"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
-  return suit[indexSuit];
+const generateRandomNumber = () => {
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  return numbers[Math.floor(Math.random() * numbers.length)];
 };
 
+const generateRandomSuit = () => {
+  const suits = ["diamond", "spade", "heart", "club"];
+  return suits[Math.floor(Math.random() * suits.length)];
+};
+
+const updateCard = card => {
+  const suit = generateRandomSuit();
+  const number = generateRandomNumber();
+
+  card.className = "card " + suit;
+  card.innerHTML = `
+    <div class="card-inner">
+      <div class="card-face card-front">
+        <span class="number">${number}</span>
+      </div>
+      <div class="card-face card-back"></div>
+    </div>
+  `;
+};
+
+const flipCard = card => {
+  card.classList.add("flip");
+
+  setTimeout(() => {
+    updateCard(card);
+    card.classList.remove("flip");
+  }, 600); // tiempo sincronizado con la animación
+};
